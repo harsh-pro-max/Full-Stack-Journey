638fe02603423fe0e857449dcdf528793274480d
Harsh kushwaha <harsh90064@gmail.com>
2025-09-03 19:10:25 +0530

    Backend Node.js learning

---
+++ BackendNode/fruits/index.js
@@ -0,0 +1,9 @@
+const banana=require("./banana");
+
+const mango=require("./mango");
+
+const orange=require("./orange");
+
+let fruits=[banana,mango,orange];
+
+module.exports=fruits;
---
+++ BackendNode/script.js
@@ -0,0 +1,19 @@
+// let n=5;
+
+// for(let i=0;i<n;i++){
+//     console.log("hellow",'' ,i);
+// }
+// let args= process.argv;
+// for(let i=2;i<args.length;i++){
+//     console.log("hello & welcome to ",args[i]);
+// }
+
+// console.log(process.argv );
+// import files information in script js
+// const someValue = require('./math');
+
+// console.log(someValue);
+
+// import folders and use files to use 
+const info =require("./fruits");
+console.log(info[0].name);
